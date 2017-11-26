57b11a6a232a8c6f44333e882e13ec4398ab4d5b
annagarbier <32601589+annagarbier@users.noreply.github.com>
2017-11-25 21:14:25 -0800

    Add modal for two additional images

--- resources/js/main.js
+++ resources/js/main.js
@@ -351,3 +351,31 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modal.style.display = "none";
 }
+
+var modal = document.getElementById('myModal');
+var img = document.getElementById('sb-3');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+var modal = document.getElementById('myModal');
+var img = document.getElementById('sb-4');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
